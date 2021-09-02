8bc4043daa8b1c14b86948be5691089a76b97f06
Julia Leuenberger <leuenberger2101@gmail.com>
2021-09-02 23:38:23 +0200

    Create questions

---
+++ app/Http/Controllers/CreateQuestionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CreateQuestionController
+{
+    public function __invoke(Request $request)
+    {
+        ddd('it works!!');
+    }
+}
